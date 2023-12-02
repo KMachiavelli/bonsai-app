b86807d41c52fa8948e4d13ab8642cff22ef3bf3
kamciaveli <>
2023-12-02 23:21:20 +0100

    texture

--- src/components/home-page/bonsai.tsx
+++ src/components/home-page/bonsai.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useFrame, useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { MeshLambertMaterial, TextureLoader } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export const Bonsai = () => {
   const obj = useLoader(GLTFLoader, "models/bonsai-w-grass.gltf");
+  const texture = useLoader(TextureLoader, "texture.jpg");
 
   useFrame((state, delta) => {
     const rotation = obj.nodes["Gleditsia_triacanthos_v02_m01"].rotation;
@@ -11,9 +14,18 @@ export const Bonsai = () => {
     rotation.x += Math.cos(state.clock.elapsedTime) * delta * 0.01;
   });
 
+  useEffect(() => {
+    (obj.nodes["pot"] as any).material = new MeshLambertMaterial({
+      map: texture,
+    });
+  }, []);
+
   return (
-    <group scale={0.2} position={[0, -10, -5]} rotation={[0.6, 0.5, 0]}>
-      <primitive object={obj.scene} />
+    <group scale={0.2} position={[0, -10, -5]} rotation={[0.65, 0.5, 0]}>
+      <primitive
+        object={obj.scene}
+        material={new MeshLambertMaterial({ color: "blue" })}
+      />
     </group>
   );
 };
--- src/components/shared/text-display.tsx
+++ src/components/shared/text-display.tsx
@@ -8,7 +8,7 @@ interface TextDisplayI {
 export const TextDisplay = ({ children, label }: TextDisplayI) => {
   return (
     <div>
-      <p className="opacity-75 border-b-2 border-slate-950 border-solid">
+      <p className="opacity-75 border-b-2 border-slate-950 border-solid text-slate-800">
         {label}
       </p>
       <p>{children}</p>
