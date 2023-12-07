398cbe4cd6f859895c8057a78a029b585cb2a5d3
kamciaveli <>
2023-12-07 01:06:40 +0100

    readjust recharts

--- src/components/dashboard/data-graph.tsx
+++ src/components/dashboard/data-graph.tsx
@@ -2,6 +2,7 @@
 
 import { Measurement } from "@prisma/client";
 import { format, parseISO } from "date-fns";
+import { pl } from "date-fns/locale";
 import {
   LineChart,
   Line,
@@ -12,6 +13,7 @@ import {
   Area,
   Tooltip,
 } from "recharts";
+import { rangeMap, unitMap } from "@/lib/measurement-utils";
 
 interface DataGraphI {
   measurements: Measurement[];
@@ -24,18 +26,31 @@ export const DataGraph = ({ measurements }: DataGraphI) => {
         .filter((key) => key !== "id" && key !== "timestamp")
         .map((key) => (
           <div key={key} className="w-full">
-            <h2 className="text-center text-rose-500 ">{key}</h2>
-            <ResponsiveContainer width={"100%"} height={300}>
+            <h2 className="text-center text-rose-400 bg-slate-800 rounded-t-lg">
+              {key}
+            </h2>
+            <ResponsiveContainer
+              width={"100%"}
+              height={300}
+              className={"bg-slate-700 opacity-80 rounded-b-lg"}
+            >
               <LineChart data={measurements}>
                 <CartesianGrid
                   stroke="#ccc"
-                  fillOpacity={0.6}
+                  fillOpacity={0.8}
                   fill="black"
                   vertical={false}
                 />
-                <Line type="basis" dataKey={key} stroke="#d88496" />
+                <Line
+                  type="basis"
+                  dataKey={key}
+                  stroke="#d88496"
+                  dot={false}
+                  strokeWidth={3}
+                />
                 <XAxis
                   dataKey="timestamp"
+                  tick={{ fill: "white" }}
                   axisLine={false}
                   tickLine={false}
                   tickFormatter={(ts) => {
@@ -46,7 +61,7 @@ export const DataGraph = ({ measurements }: DataGraphI) => {
                     return "";
                   }}
                 />
-                <YAxis />
+                <YAxis tick={{ fill: "white" }} domain={rangeMap.get(key)} />
                 <Tooltip content={<CustomTooltip attr={key} />} />
               </LineChart>
             </ResponsiveContainer>
@@ -61,10 +76,18 @@ function CustomTooltip({ active, payload, label, attr }: any) {
     return (
       <div className="opacity-70 text-slate-50">
         <p>
-          {attr} <b>{payload[0].payload[attr]}%</b>
+          {attr}{" "}
+          <b>
+            {payload[0].payload[attr]}
+            {unitMap.get(attr)}
+          </b>
         </p>
         <p>
-          {format(new Date(payload[0].payload.timestamp), "eeee, d MMM, yyyy")}
+          {format(
+            new Date(payload[0].payload.timestamp),
+            "eeee, d MMM, yyyy HH:mm",
+            { locale: pl }
+          )}
         </p>
       </div>
     );
---
+++ src/lib/measurement-utils.ts
@@ -0,0 +1,13 @@
+export const unitMap = new Map([
+  ["temperature", "°C"],
+  ["humidity", "%"],
+  ["moisture", "%"],
+  ["light", ""],
+]);
+
+export const rangeMap = new Map([
+  ["temperature", [-20, 40]],
+  ["humidity", [0, 100]],
+  ["moisture", [0, 100]],
+  ["light", [0, 4096]],
+]);
